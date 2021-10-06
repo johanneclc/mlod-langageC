0d74ac87c6df6d6d3b9908c61b6bf271b6f12ac9
johanneclc <62894386+johanneclc@users.noreply.github.com>
2021-10-06 08:53:01 +0000

    TP1 en cours

--- workspace/hello.c
+++ workspace/hello.c
@@ -3,11 +3,20 @@
 int a = 2;
 
 int main(void) {
-    int a = 3;
 
     printf("hello");
-
-    a = 4;
+    printf(15,5);
 
     return 0;
 }
+
+float prixBillet(prix_plein_tarif, age){
+    if(age<=2)
+        return prix_plein_tarif*0,1;
+    else if(age<=12)
+        return prix_plein_tarif*0,5;
+    else
+        return prix_plein_tarif;    
+}
+
+valeursDés
