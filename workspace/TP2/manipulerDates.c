23f6a2cca03d04cd891acc8ac87e5f88c7f62527
johanneclc <62894386+johanneclc@users.noreply.github.com>
2021-10-11 13:37:46 +0000

    TP2

--- workspace/TP2/manipulerDates.c
+++ workspace/TP2/manipulerDates.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 typedef enum {jan = 1, fev, mars, avr, mai, juin,
     juil, aout, sept, oct, nov, dec} Mois;
@@ -13,19 +15,12 @@ struct DateSimple {
 typedef struct DateSimple Date;
 
 void initialiseDate(Date *d){
-    int jour, annee; 
-    Mois mois; 
     printf("Jour (compris entre 1 et 31) :"); 
-    scanf( "%i",&jour ); 
+    scanf( "%i",&d->jour ); 
     printf("Mois (jan=1, fev=2,...) : "); 
-    scanf( "%i",&mois ); 
+    scanf( "%i",(int *) &d->mois ); 
     printf("Année :"); 
-    scanf( "%i",&annee ); 
-    
-    (*d).jour = jour; 
-    //(*d).mois = mois; 
-    d->mois = mois;
-    (*d).annee = annee; 
+    scanf( "%i",&d->annee ); 
     
 }
 
@@ -41,35 +36,57 @@ Date creerDateParCopie(){
     return date; 
 }
 
-int newDate(){
-    int adresseJour = (int *) malloc(sizeof(int));
-    int adresseMois = (int *) malloc(sizeof(int));
-    int adresseAnnee=  (int *) malloc(sizeof(int));
+void *newDate(){
+    struct DateSimple *adresse = NULL ; 
+    adresse = malloc(sizeof(struct DateSimple));
 
-    *adresseJour =  (int *) malloc(sizeof(int));
-    *adresseMois =  (int *) malloc(sizeof(int));
-    *adresseAnnee =  (int *) malloc(sizeof(int));
+    if(adresse){
+        printf("Jour (compris entre 1 et 31) :"); 
+        scanf( "%i",&adresse->jour ); 
+        printf("Mois (jan=1, fev=2,...) : "); 
+        scanf( "%i",(int *) &adresse->mois ); 
+        printf("Année :"); 
+        scanf( "%i",&adresse->annee );
+
+        return adresse;
+    }
+    else 
+        return 0; 
 }
+
+unsigned int nbreJours(Mois mois, unsigned int annee){
+    if(mois == 2 && annee%4 != 0)
+        return 28 ; 
+    if(mois == 2 && annee%4 == 0)
+        return 29 ; 
+    else if(mois % 2 == 0)
+        return 30; 
+    else
+        return 31; 
+}
+
+bool dateValide(Date uneDate){
+    return (uneDate.jour>0 && uneDate.jour < nbreJours(uneDate.mois, uneDate.annee));
+}
+
 
 int main(void) {
 
+    //version avec modification par pointeur 
     Date d; 
-
-    initialiseDate(&d); //Pourquoi a t-on ajouté un &?
+    initialiseDate(&d); 
     afficheDate(&d);
 
+    // version copie de la mémoire 
     Date date;
-    date = creerDateParCopie();
+    date = creerDateParCopie(); //Création de 3 cases mémoire supplémentaires donc moins optimal
     afficheDate(&date);
 
+    // version allocation manuelle d'un espace mémoire 
     Date *date2;
-    date = newDate();
+    date2 = newDate();
     afficheDate(date2);
-    //...
     free(date2);
-
-
-
 
     return EXIT_SUCCESS;
 }
